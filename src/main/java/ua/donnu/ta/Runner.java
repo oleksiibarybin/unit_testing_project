363094eac95a466d249d6413a97b8d6d5c661d6c
Oleksii <64740524+oleksiibarybin@users.noreply.github.com>
2021-02-04 19:17:32 +0200

    Area methods for int and float implemented

--- src/main/java/ua/donnu/ta/Runner.java
+++ src/main/java/ua/donnu/ta/Runner.java
@@ -1,10 +1,19 @@
 package ua.donnu.ta;
 
+import ua.donnu.ta.square.Square;
+
 public class Runner {
 
     public static void main(String[] args) {
 
+        int result = 0;
 
+        int sideFirst = 2;
+        int sideSecond = 4;
+
+        result = Square.area(sideFirst, sideSecond);
+
+        System.out.println(result);
     }
 
 }
