05f4e1c3586b479595b02db53e1c3d10fc56fe67
666PiLiGriM999 <ihor12345669@gmail.com>
2021-05-27 16:18:11 +0300

    Cylinder surface area and volume calc testing

--- src/main/java/ua/donnu/ta/Cylinder/Cylinder.java
+++ src/main/java/ua/donnu/ta/Cylinder/Cylinder.java
@@ -4,28 +4,28 @@ public class Cylinder {
 
     public static int surfaceArea (int height, int radius){
         if (height<= 0 | radius <= 0 ){
-            return 0;
+            return 1;
         }
         return (int) ((2*3.14*radius*height) + (2*3.14*radius*radius));
     }
 
     public static int cylinderVolume(int height, int radius){
         if (height<= 0 | radius <= 0 ) {
-            return 0;
+            return 1;
         }
         return ((int) 3.14*radius*radius*height);
     }
 
     public static float surfaceArea (float height, float radius){
         if (height<= 0 | radius <= 0 ){
-            return 0;
+            return 1;
         }
         return (float) ((2*3.14*radius*height) + (2*3.14*radius*radius));
     }
 
     public static float cylinderVolume(float height, float radius){
         if (height<= 0 | radius <= 0 ) {
-            return 0;
+            return 1;
         }
         return ((float) 3.14*radius*radius*height);
     }
--- src/test/java/ua/donnu/ta/CylinderTestClass.java
+++ src/test/java/ua/donnu/ta/CylinderTestClass.java
@@ -9,9 +9,61 @@ public class CylinderTestClass {
 
     @Test
     @DisplayName("Testing of surface area calculation for positive integer numbers")
-    public void checkIfSurfaceAreaCalculationForPositiveIntNumbersIsCorrect(){
-        Assertions.assertEquals(628, Cylinder.surfaceArea(15,5));
+    public void checkIfSurfaceAreaCalculationForPositiveIntNumbersIsCorrect() {
+        Assertions.assertEquals(628, Cylinder.surfaceArea(15, 5));
     }
 
+    @Test
+    @DisplayName("Testing of surface area calculation for negative integer numbers")
+    public void checkIfSurfaceAreaCalculationForNegativeIntNumbersIsCorrect() {
+        Assertions.assertEquals(1, Cylinder.surfaceArea(-15, -5));
+    }
 
+    @Test
+    @DisplayName("Testing of surface area calculation for positive float numbers")
+    public void checkIfSurfaceAreaCalculationForPositiveFloatNumbersIsCorrect() {
+        Assertions.assertEquals(628.0, Cylinder.surfaceArea(15f, 5f));
+    }
+
+    @Test
+    @DisplayName("Testing of surface area calculation for negative float numbers")
+    public void checkIfSurfaceAreaCalculationForNegativeFloatNumbersIsCorrect() {
+        Assertions.assertEquals(1, Cylinder.surfaceArea(-15f, -5f));
+    }
+
+    @Test
+    @DisplayName("Testing of surface area calculation for zero numbers")
+    public void checkIfSurfaceAreaCalculationForZeroNumbersIsCorrect() {
+        Assertions.assertEquals(1, Cylinder.surfaceArea(0, -5));
+    }
+
+    @Test
+    @DisplayName("Testing of volume calculation for positive integer numbers")
+    public void checkIfVolumeCalculationForPositiveIntNumbersIsCorrect() {
+        Assertions.assertEquals(1125, Cylinder.cylinderVolume(15, 5));
+    }
+
+    @Test
+    @DisplayName("Testing of volume calculation for negative integer numbers")
+    public void checkIfVolumeCalculationForNegativeIntNumbersIsCorrect() {
+        Assertions.assertEquals(1, Cylinder.cylinderVolume(-15, -5));
+    }
+
+    @Test
+    @DisplayName("Testing of volume calculation for positive float numbers")
+    public void checkIfVolumeCalculationForPositiveFloatNumbersIsCorrect() {
+        Assertions.assertEquals(1177.5, Cylinder.cylinderVolume(15f, 5f));
+    }
+
+    @Test
+    @DisplayName("Testing of volume calculation for negative float numbers")
+    public void checkIfVolumeCalculationForNegativeFloatNumbersIsCorrect() {
+        Assertions.assertEquals(1, Cylinder.cylinderVolume(-15f, -5f));
+    }
+
+    @Test
+    @DisplayName("Testing of volume calculation for zero numbers")
+    public void checkIfVolumeCalculationForZeroNumbersIsCorrect() {
+        Assertions.assertEquals(1, Cylinder.cylinderVolume(15, 0));
+    }
 }
